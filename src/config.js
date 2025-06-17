b9072adc5c614dda8f6e3948f5e984fd42790915
Nitesh Bisht <niteshkumar@paradisetechsoft.com>
2025-06-17 15:57:59 +0530

    Update

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_URL: process.env.REACT_APP_API_URL || 'https://python-backend-03i5.onrender.com'
+    API_URL: process.env.REACT_APP_API_URL || 'http://localhost:8000'
     // API_URL: 'http://localhost:8000'
     };
 
